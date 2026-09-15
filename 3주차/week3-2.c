fac7b54437cebad98332bcf23b9ee951c80d7444
parksae07 <gaeun2633@gmail.com>
2026-09-15 12:31:02 +0900

    3주차

---
+++ 3주차/week3-2.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    scanf("%d", &num);
+
+    printf("%s", (num % 2==0)?"Even":"Odd");
+    return 0;
+}
---
+++ 3주차/week3-4.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int numA, numB;
+    scanf("%d", &numA);
+    scanf("%d", &numB);
+
+    int temp = numB;
+
+    while(temp > 0){
+        printf("%d\n", numA*(temp % 10));
+        temp /= 10;
+    }
+    printf("%d", numA*numB);
+    return 0;
+}
